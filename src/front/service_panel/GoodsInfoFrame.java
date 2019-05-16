cd6a47861d19305cdb441f9fd396d3a1c18928f8
AbsoLab <kljj0650@naver.com>
2019-05-16 13:48:46 +0900

    GoodsList

--- src/front/service_panel/GoodsInfoFrame.java
+++ src/front/service_panel/GoodsInfoFrame.java
@@ -3,9 +3,13 @@ package front.service_panel;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -21,6 +25,7 @@ public class GoodsInfoFrame extends JDialog {
 	private JPanel image_panel;
 	private JPanel goods_info_panel;
 	private JLabel show_detailed_spec_label;
+	private JButton [] button;
 	
 	private MainFrame mf;
 	private Set set;
@@ -51,12 +56,25 @@ public class GoodsInfoFrame extends JDialog {
 		
 		/*상세 스펙 버튼*/
 		if (goods.is_smartphone()) {
-			show_detailed_spec_label = new JLabel("+");
+			show_detailed_spec_label = new JLabel("상세 정보");
 			show_detailed_spec_label.setHorizontalAlignment(JLabel.CENTER);
 			show_detailed_spec_label.setBorder(new LineBorder(new Color(109, 109, 255), 2));
-			show_detailed_spec_label.setBounds(660, 340, 20, 20);
+			show_detailed_spec_label.setBounds(620, 340, 60, 20);
 			show_detailed_spec_label.addMouseListener(new MouseActionListener());
 			add(show_detailed_spec_label);
+		}
+		
+		/*여러 버튼 생성*/
+		String [] button_name = {"장바구니 담기", "닫기"};
+		button = new JButton[button_name.length];
+		for (int i=0; i<button.length; ++i	 ) {
+			button[i] = new JButton(button_name[i]);
+			button[i].setBackground(Color.WHITE);
+			button[i].setFont(new Font("맑은 고딕", Font.BOLD, 15));
+			button[i].setMargin(new Insets(0, 0, 0, 0));
+			button[i].setBounds(430 + 130*i, 400, 120, 30);
+			button[i].addActionListener(new ButtonEventListener());
+			add(button[i]);
 		}
 		
 		setResizable(false);
@@ -138,7 +156,7 @@ public class GoodsInfoFrame extends JDialog {
 			
 			for (int i=0; i<right_label.length; ++i) {
 				right_label[i].setFont(new Font("맑은 고딕", Font.BOLD, 13));
-				right_label[i].setBounds(120, 20 + 30 * i, 90, 30);
+				right_label[i].setBounds(120, 20 + 30 * i, 120, 30);
 				add(right_label[i]);
 			}
 			
@@ -157,6 +175,26 @@ public class GoodsInfoFrame extends JDialog {
 			// TODO Auto-generated method stub	
 			if (goods.is_smartphone())
 				new DetailedSpecificationFrame(mf, (Smartphone)goods);
+		}
+		
+	}
+
+	/*버튼 이벤트 리스너*/
+	class ButtonEventListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			
+			JButton btn = (JButton)e.getSource();
+			switch(btn.getText()) {
+			case "장바구니 담기":
+				set.AddKart(goods.get_goods_id(), 1);
+				break;
+			case "닫기":
+				dispose();
+				break;
+			}
 		}
 		
 	}
